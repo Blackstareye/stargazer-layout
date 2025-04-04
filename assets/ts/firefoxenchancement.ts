6a4860d1f5bbabda5e9f0d9c66e29e43a2e8671e
Blackeye <>
2025-04-04 06:33:28 +0200

    fixed layouting and minor bugs

--- assets/ts/firefoxenchancement.ts
+++ assets/ts/firefoxenchancement.ts
@@ -1,6 +1,6 @@
 if (navigator.userAgent.includes("Firefox")) {
     document.querySelectorAll(".str-text-fx").forEach( (e ) => {
         (e as HTMLElement).classList.add("str-text-fx-firefox");
-        console.debug("Firefox Debug: applied Class");
+        // console.debug("Firefox Debug: applied Class");
     });
 }
